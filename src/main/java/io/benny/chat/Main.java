7b680b3111e5639c77f489048ce00be39185db95
Benny Zlotnik <uber442@gmail.com>
2016-09-13 19:28:32 +0300

    Starting webapp

---
+++ src/main/java/io/benny/chat/Main.java
@@ -0,0 +1,10 @@
+package io.benny.chat;
+
+/**
+ * Created by benny on 9/13/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/webapp/webpack.config.js
@@ -0,0 +1,31 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+       './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, excluse: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'less-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+
+}
+
